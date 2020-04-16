2988aa1ddfd4c13be136a1319b1946e12c847ae0
SOFTTEKCLIENTS\pedro.carrazco <pcarrazco@24hourfit.com>
2020-04-16 15:07:24 -0500

    Controller CSS shadows

--- src/components/Control/presentational/Controller.tsx
+++ src/components/Control/presentational/Controller.tsx
@@ -2,9 +2,10 @@ import { styled } from 'linaria/react'
 
 const Controller = styled.div`
 	background-color: #d0cfd5;
-	border: 2px solid #d0cfd5;
 	border-radius: 10px;
 	bottom: 5%;
+	box-shadow: inset -1px -1px 2px #92928f, inset 1px 1px 2px #efefef,
+		4px 4px 4px #676767, 8px 8px 15px #d0cfd5;
 	display: flex;
 	height: 150px;
 	justify-content: center;
